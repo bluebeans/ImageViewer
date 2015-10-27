a1ae96a827c6e453608476d53686ab6eb234112b
Ni Yan <maomaochonghome@gmail.com>
2015-10-27 16:19:28 -0400

    initial

---
+++ ImageViewer/ImageCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageCollectionViewController.h
+//  ImageViewer
+//
+//  Created by Ni Yan on 10/27/15.
+//  Copyright © 2015 Ni Yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageCollectionViewController : UICollectionViewController
+
+@end
